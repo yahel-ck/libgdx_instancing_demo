3232a73abc0fcba8b53c229c678e6af76f07c410
Yahel Cohen <yahelcohenko@gmail.com>
2024-09-27 20:51:29 +0300

    Add shadows

--- core/src/com/yahelck/test3d/Test3DGame.kt
+++ core/src/com/yahelck/test3d/Test3DGame.kt
@@ -6,27 +6,33 @@ import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter
-import com.badlogic.gdx.graphics.g3d.Model
+import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
-import com.badlogic.gdx.graphics.g3d.model.Node
+import com.badlogic.gdx.graphics.g3d.shaders.DepthShader
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.graphics.glutils.InstanceBufferObject
 import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.BufferUtils
 import net.mgsx.gltf.loaders.gltf.GLTFLoader
 import net.mgsx.gltf.scene3d.attributes.FogAttribute
 import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute
 import net.mgsx.gltf.scene3d.attributes.PBRTextureAttribute
 import net.mgsx.gltf.scene3d.lights.DirectionalLightEx
+import net.mgsx.gltf.scene3d.lights.DirectionalShadowLight
 import net.mgsx.gltf.scene3d.scene.Scene
 import net.mgsx.gltf.scene3d.scene.SceneAsset
 import net.mgsx.gltf.scene3d.scene.SceneManager
 import net.mgsx.gltf.scene3d.scene.SceneSkybox
+import net.mgsx.gltf.scene3d.shaders.PBRDepthShaderProvider
 import net.mgsx.gltf.scene3d.shaders.PBRShaderConfig
 import net.mgsx.gltf.scene3d.shaders.PBRShaderProvider
 import net.mgsx.gltf.scene3d.utils.IBLBuilder
+import java.nio.IntBuffer
+import kotlin.math.sign
 import kotlin.random.Random
 
 
@@ -34,7 +40,6 @@ class Test3DGame : Game() {
     private val camera = PerspectiveCamera()
     private var cameraController: CameraInputController? = null
     private val rand = Random(123456789L)
-    private val v = Vector3()
 
     private var sceneManager: SceneManager? = null
     private var sceneAsset: SceneAsset? = null
@@ -46,17 +51,24 @@ class Test3DGame : Game() {
     override fun create() {
         setupCamera()
         cameraController = CameraInputController(camera)
+        cameraController?.target?.set(camera.position)
         Gdx.input.inputProcessor = cameraController
-        createSceneManager()
-        sceneManager!!.camera = camera
-        createModels()
-        sceneManager!!.addScene(Scene(sceneAsset!!.scene))
+        val sceneManager = createSceneManager()
+        sceneManager.camera = camera
+        loadModels()
+        if (USE_INSTANCED_RENDERING) {
+            sceneManager.addScene(getRandomInstancedScene())
+        } else {
+            repeat(INSTANCE_COUNT) {
+                sceneManager.addScene(getRandomScene())
+            }
+        }
         createScreen()
     }
 
     private fun setupCamera() {
         camera.apply {
-            fieldOfView = 80f
+            fieldOfView = 100f
             viewportWidth = Gdx.graphics.width.toFloat()
             viewportHeight = Gdx.graphics.height.toFloat()
             near = 0.2f
@@ -66,13 +78,18 @@ class Test3DGame : Game() {
         }
     }
 
-    private fun createSceneManager() {
+    private fun createSceneManager(): SceneManager {
+        val lightDir = Vector3(-0.2f, -1f, -1f).nor()
+        val shadowBoundSide = SPAWN_BOX_SIDE * 1.1f
+        val shadowLight = DirectionalShadowLight(4096, 4096, shadowBoundSide,  shadowBoundSide, 0f, shadowBoundSide)
+        shadowLight.set(1f, 1f, 0.9f, lightDir)
+        shadowLight.intensity = 3f
+
         // setup quick IBL (image based lighting)
-        val light = DirectionalLightEx().set(1f, 1f, 1f, -1f, -0.8f, 0.2f)
         val fogColor = Color(1f, 1f, 1f, 1f)
-        val iblBuilder = IBLBuilder.createOutdoor(light).apply {
+        val iblBuilder = IBLBuilder.createOutdoor(shadowLight).apply {
             farGroundColor.set(fogColor) // .set(0.94f, 0.40f, 0.36f, 1f)
-            nearGroundColor.set(0.71f, 0.42f, 0.34f, 1f)
+            nearGroundColor.set(0.99f, 0.5f, 0.4f, 1f)
             farSkyColor.set(fogColor) // .set(.9f, .95f, 1f, 1f)
             nearSkyColor.set(.7f, .8f, 1f, 1f)
         }
@@ -84,44 +101,71 @@ class Test3DGame : Game() {
         brdfLUT = Texture(Gdx.files.classpath("net/mgsx/gltf/shaders/brdfLUT.png"))
 
         val shaderProviderConfig = PBRShaderConfig().apply {
-            vertexShader = Gdx.files.internal("instancing_pbr.vs.glsl").readString()
+            vertexShader = Gdx.files.internal("shaders/instancing_pbr.vs.glsl").readString()
             fragmentShader = PBRShaderProvider.getDefaultFragmentShader()
-            numBones = 24
+            numBones = 4
+            numDirectionalLights = 1
+            numPointLights = 0
+            numSpotLights = 0
         }
+        val shaderProvider = PBRShaderProvider.createDefault(shaderProviderConfig)
 
-        sceneManager = SceneManager(
-            PBRShaderProvider.createDefault(shaderProviderConfig),
-            PBRShaderProvider.createDefaultDepth(24)
-        ).apply {
-            setAmbientLight(1f)
+        val depthProviderConfig = DepthShader.Config().apply {
+            vertexShader = Gdx.files.internal("shaders/instancing_depth.vs.glsl").readString()
+            fragmentShader = PBRDepthShaderProvider.getDefaultFragmentShader();
+            numBones = 4
+            numDirectionalLights = 1
+            numPointLights = 0
+            numSpotLights = 0
+        }
+        val depthProvider = PBRShaderProvider.createDefaultDepth(depthProviderConfig)
+
+        val sceneManager = SceneManager(shaderProvider, depthProvider).apply {
+            setAmbientLight(0.9f)
             environment.apply {
                 set(PBRTextureAttribute(PBRTextureAttribute.BRDFLUTTexture, brdfLUT))
                 set(PBRCubemapAttribute.createSpecularEnv(specularCubemap))
                 set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap))
                 set(ColorAttribute(ColorAttribute.Fog, fogColor))
                 set(FogAttribute(FogAttribute.FogEquation).set(3f, 130f, 0.8f))
+                add(shadowLight)
             }
             skyBox = SceneSkybox(envCubemap)
         }
+
+        this.sceneManager = sceneManager
+        return sceneManager
     }
 
-    private fun createModels() {
+    private fun loadModels() {
         sceneAsset = loadScene()
-        val model: Model = sceneAsset!!.scene.model
-        model.calculateTransforms()
+    }
 
+    private fun getRandomScene(): Scene {
+        val scene = Scene(sceneAsset!!.scene)
+        setRandomTransform(scene.modelInstance.transform)
+        return scene
+    }
+
+    private fun getRandomInstancedScene(): Scene {
+        val scene = Scene(sceneAsset!!.scene)
+        enableInstancedRendering(scene.modelInstance)
+        return scene
+    }
+
+    private fun enableInstancedRendering(model: ModelInstance) {
         val mat = Matrix4()
+        val instances =  InstanceBufferObject(true, INSTANCE_COUNT, *getMatrix4Attributes())
+        instances.numInstances = INSTANCE_COUNT
+
         val transforms = FloatArray(INSTANCE_COUNT * MAT_FLOAT_COUNT)
         repeat(INSTANCE_COUNT) { index ->
             setRandomTransform(mat)
             mat.values.copyInto(transforms, index * MAT_FLOAT_COUNT)
         }
 
-        model.meshes.forEach {
-            it.enableInstancedRendering(true, INSTANCE_COUNT, *getMatrix4Attributes())
-            it.setInstanceData(transforms)
-            Gdx.app.log("InstancedRendering", "Mesh attributes: ${it.vertexAttributes}")
-        }
+        instances.setInstanceData(transforms, 0, transforms.size)
+        model.instances = instances
     }
 
     private fun setRandomTransform(out: Matrix4) {
@@ -131,10 +175,15 @@ class Test3DGame : Game() {
             rand.nextFloat(),
             rand.nextFloat() * 360f
         )
-        val x = (rand.nextFloat() - 0.5f) * SPAWN_BOX_SIDE
-        val y = (rand.nextFloat() - 0.5f) * SPAWN_BOX_SIDE
-        val z = (rand.nextFloat() - 0.5f) * SPAWN_BOX_SIDE
+        val x = getRandomCoordinate()
+        val y = getRandomCoordinate()
+        val z = getRandomCoordinate()
         out.translate(x, y, z)
+    }
+
+    private fun getRandomCoordinate(): Float {
+        val scl = rand.nextFloat() - 0.5f
+        return SPAWN_BOX_INNER_SPACE / 2f * scl.sign + scl * (SPAWN_BOX_SIDE - SPAWN_BOX_INNER_SPACE)
     }
 
     private fun createScreen() {
@@ -155,10 +204,11 @@ class Test3DGame : Game() {
     override fun render() {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST)
         cameraController?.apply {
-            v.set(camera.direction).nor().scl(CAMERA_SPEED * Gdx.graphics.deltaTime)
-            target.add(v)
-            camera.position.add(v)
+            //v.set(camera.direction).nor().scl(CAMERA_SPEED * Gdx.graphics.deltaTime)
+            //target.add(v)
+            //camera.position.add(v)
             camera.update()
         }
         sceneManager?.apply {
@@ -176,8 +226,21 @@ class Test3DGame : Game() {
     companion object {
         const val MAT_FLOAT_COUNT = 4 * 4
         const val CAMERA_SPEED = 16f
-        const val SPAWN_BOX_SIDE = 200
-        const val INSTANCE_COUNT: Int = SPAWN_BOX_SIDE * SPAWN_BOX_SIDE * SPAWN_BOX_SIDE / 2000
+        const val SPAWN_BOX_SIDE = 20
+        const val SPAWN_BOX_INNER_SPACE = 6
+        const val INSTANCE_COUNT: Int = SPAWN_BOX_SIDE * SPAWN_BOX_SIDE * SPAWN_BOX_SIDE / 200
+        const val DRAW_COMMAND_INT_COUNT: Int = 5
+        const val DRAW_COMMAND_BYTE_COUNT: Int = DRAW_COMMAND_INT_COUNT * 4
+        const val USE_INSTANCED_RENDERING = false
+
+        private fun IntBuffer.putDrawCommand(count: Int, instanceCount: Int, firstIndex: Int, baseVertex: Int) {
+            // Last value must be zero (reserved)
+            this.put(
+                intArrayOf(
+                    count, instanceCount, firstIndex, baseVertex, 0
+                )
+            )
+        }
 
         private fun getMatrix4Attributes(): Array<VertexAttribute> {
             return Array(4) { index ->
