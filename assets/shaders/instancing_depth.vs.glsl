debb8be42fed9ee72875805d32600a05f0986dd7
Yahel Cohen <yahelcohenko@gmail.com>
2024-09-28 12:18:29 +0300

    Upgrade gdx-gltf to 2.2.1 + add shadows + add floor

--- assets/shaders/instancing_depth.vs.glsl
+++ assets/shaders/instancing_depth.vs.glsl
@@ -1,3 +1,24 @@
+#line 1
+
+//#include <compat.vs.glsl>
+// required to have same precision in both shader for light structure
+#ifdef GL_ES
+#define LOWP lowp
+#define MED mediump
+#define HIGH highp
+precision highp float;
+#else
+#define MED
+#define LOWP
+#define HIGH
+#endif
+
+#ifdef GLSL3
+#define attribute in
+#define varying out
+#endif
+
+
 attribute vec3 a_position;
 
 #ifdef a_worldTrans_instancedFlag
@@ -5,7 +26,12 @@ uniform mat4 u_projViewTrans;
 uniform mat4 u_worldTrans;
 attribute mat4 a_worldTrans;
 #else
+	#ifdef a_worldTrans_bullet3FormatFlag
+uniform mat4 u_projViewTrans;
+uniform mat4 u_worldTrans;
+#else
 uniform mat4 u_projViewWorldTrans;
+#endif
 #endif
 
 #ifdef position0Flag
@@ -130,73 +156,103 @@ varying float v_depth;
 #endif //PackedDepthFlag
 
 void main() {
-    #ifdef blendedTextureFlag
+	#ifdef blendedTextureFlag
 		v_texCoords0 = a_texCoord0;
-    #endif // blendedTextureFlag
+	#endif // blendedTextureFlag
 
-    #ifdef skinningFlag
+	#ifdef skinningFlag
 		mat4 skinning = mat4(0.0);
-    #ifdef boneWeight0Flag
+	#ifdef boneWeight0Flag
 			skinning += (a_boneWeight0.y) * u_bones[int(a_boneWeight0.x)];
-    #endif //boneWeight0Flag
+	#endif //boneWeight0Flag
 		#ifdef boneWeight1Flag
 			skinning += (a_boneWeight1.y) * u_bones[int(a_boneWeight1.x)];
-    #endif //boneWeight1Flag
+	#endif //boneWeight1Flag
 		#ifdef boneWeight2Flag
 			skinning += (a_boneWeight2.y) * u_bones[int(a_boneWeight2.x)];
-    #endif //boneWeight2Flag
+	#endif //boneWeight2Flag
 		#ifdef boneWeight3Flag
 			skinning += (a_boneWeight3.y) * u_bones[int(a_boneWeight3.x)];
-    #endif //boneWeight3Flag
+	#endif //boneWeight3Flag
 		#ifdef boneWeight4Flag
 			skinning += (a_boneWeight4.y) * u_bones[int(a_boneWeight4.x)];
-    #endif //boneWeight4Flag
+	#endif //boneWeight4Flag
 		#ifdef boneWeight5Flag
 			skinning += (a_boneWeight5.y) * u_bones[int(a_boneWeight5.x)];
-    #endif //boneWeight5Flag
+	#endif //boneWeight5Flag
 		#ifdef boneWeight6Flag
 			skinning += (a_boneWeight6.y) * u_bones[int(a_boneWeight6.x)];
-    #endif //boneWeight6Flag
+	#endif //boneWeight6Flag
 		#ifdef boneWeight7Flag
 			skinning += (a_boneWeight7.y) * u_bones[int(a_boneWeight7.x)];
-    #endif //boneWeight7Flag
+	#endif //boneWeight7Flag
 	#endif //skinningFlag
 
-    #ifdef morphTargetsFlag
+	#ifdef morphTargetsFlag
 		vec3 morph_pos = a_position;
-    #ifdef position0Flag
+	#ifdef position0Flag
 			morph_pos += a_position0 * u_morphTargets1.x;
-    #endif
+	#endif
 		#ifdef position1Flag
 			morph_pos += a_position1 * u_morphTargets1.y;
-    #endif
+	#endif
 		#ifdef position2Flag
 			morph_pos += a_position2 * u_morphTargets1.z;
-    #endif
+	#endif
 		#ifdef position3Flag
 			morph_pos += a_position3 * u_morphTargets1.w;
-    #endif
+	#endif
 		#ifdef position4Flag
 			morph_pos += a_position4 * u_morphTargets2.x;
-    #endif
+	#endif
 		#ifdef position5Flag
 			morph_pos += a_position5 * u_morphTargets2.y;
-    #endif
+	#endif
 		#ifdef position6Flag
 			morph_pos += a_position6 * u_morphTargets2.z;
-    #endif
+	#endif
 		#ifdef position7Flag
 			morph_pos += a_position7 * u_morphTargets2.w;
-    #endif
+	#endif
 	#else
 		vec3 morph_pos = a_position;
-    #endif
+	#endif
 
 	#ifdef a_worldTrans_instancedFlag
-        #define profViewWorldTransExp0 u_projViewTrans * a_worldTrans * u_worldTrans
-    #else
-        #define profViewWorldTransExp0 u_projViewWorldTrans
-	#endif //a_worldTrans_instancedFlag
+		#define i_worldTrans a_worldTrans
+	#else
+		#define i_worldTrans u_worldTrans
+	#endif
+
+	#ifdef a_worldTrans_bullet3FormatFlag
+    mat4 var_worldTrans = mat4(1.0);
+	var_worldTrans[0][0] = i_worldTrans[0][0];
+	var_worldTrans[0][1] = i_worldTrans[1][0];
+	var_worldTrans[0][2] = i_worldTrans[2][0];
+
+	var_worldTrans[1][0] = i_worldTrans[0][1];
+	var_worldTrans[1][1] = i_worldTrans[1][1];
+	var_worldTrans[1][2] = i_worldTrans[2][1];
+
+	var_worldTrans[2][0] = i_worldTrans[0][2];
+	var_worldTrans[2][1] = i_worldTrans[1][2];
+	var_worldTrans[2][2] = i_worldTrans[2][2];
+
+	var_worldTrans[3][0] = i_worldTrans[3][0];
+	var_worldTrans[3][1] = i_worldTrans[3][1];
+	var_worldTrans[3][2] = i_worldTrans[3][2];
+		#ifdef a_worldTrans_instancedFlag
+			#define profViewWorldTransExp0 u_projViewTrans * var_worldTrans * u_worldTrans
+		#else
+			#define profViewWorldTransExp0 u_projViewTrans * var_worldTrans
+		#endif
+    #else //a_worldTrans_bullet3FormatFlag
+		#ifdef a_worldTrans_instancedFlag
+			#define profViewWorldTransExp0 u_projViewTrans * a_worldTrans * u_worldTrans
+		#else
+			#define profViewWorldTransExp0 u_projViewWorldTrans
+		#endif //a_worldTrans_instancedFlag
+	#endif //a_worldTrans_bullet3FormatFlag
 
 	#ifdef skinningFlag
 		#define profViewWorldTransExp1 profViewWorldTransExp0 * skinning
@@ -204,11 +260,11 @@ void main() {
         #define profViewWorldTransExp1 profViewWorldTransExp0
 	#endif //skinningFlag
 
-    vec4 pos = profViewWorldTransExp1 * vec4(morph_pos, 1.0);
+	vec4 pos = profViewWorldTransExp1 * vec4(morph_pos, 1.0);
 
-    #ifdef PackedDepthFlag
+	#ifdef PackedDepthFlag
 		v_depth = pos.z / pos.w * 0.5 + 0.5;
-    #endif //PackedDepthFlag
+	#endif //PackedDepthFlag
 
-    gl_Position = pos;
+	gl_Position = pos;
 }
--- core/src/com/yahelck/test3d/Test3DGame.kt
+++ core/src/com/yahelck/test3d/Test3DGame.kt
@@ -1,5 +1,6 @@
 package com.yahelck.test3d
 
+import com.badlogic.gdx.Application.ApplicationType
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.*
@@ -16,12 +17,12 @@ import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Align
-import com.badlogic.gdx.utils.BufferUtils
 import net.mgsx.gltf.loaders.gltf.GLTFLoader
 import net.mgsx.gltf.scene3d.attributes.FogAttribute
 import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute
+import net.mgsx.gltf.scene3d.attributes.PBRFloatAttribute
+import net.mgsx.gltf.scene3d.attributes.PBRFloatAttribute.ShadowBias
 import net.mgsx.gltf.scene3d.attributes.PBRTextureAttribute
-import net.mgsx.gltf.scene3d.lights.DirectionalLightEx
 import net.mgsx.gltf.scene3d.lights.DirectionalShadowLight
 import net.mgsx.gltf.scene3d.scene.Scene
 import net.mgsx.gltf.scene3d.scene.SceneAsset
@@ -40,9 +41,11 @@ class Test3DGame : Game() {
     private val camera = PerspectiveCamera()
     private var cameraController: CameraInputController? = null
     private val rand = Random(123456789L)
+    private val tmpV = Vector3()
 
     private var sceneManager: SceneManager? = null
-    private var sceneAsset: SceneAsset? = null
+    private var pillarSceneAsset: SceneAsset? = null
+    private var cubeSceneAsset: SceneAsset? = null
     private var brdfLUT: Texture? = null
 
     private var font: BitmapFont? = null
@@ -55,15 +58,14 @@ class Test3DGame : Game() {
         Gdx.input.inputProcessor = cameraController
         val sceneManager = createSceneManager()
         sceneManager.camera = camera
+
         loadModels()
-        if (USE_INSTANCED_RENDERING) {
-            sceneManager.addScene(getRandomInstancedScene())
-        } else {
-            repeat(INSTANCE_COUNT) {
-                sceneManager.addScene(getRandomScene())
-            }
-        }
+        addScene(pillarSceneAsset!!, INSTANCE_COUNT, getFloorTransforms(INSTANCE_COUNT))
+        addScene(cubeSceneAsset!!, 10, getPillarTransforms(10))
+
         createScreen()
+
+        Gdx.app.log("TestInstance", "Finished create")
     }
 
     private fun setupCamera() {
@@ -79,9 +81,9 @@ class Test3DGame : Game() {
     }
 
     private fun createSceneManager(): SceneManager {
-        val lightDir = Vector3(-0.2f, -1f, -1f).nor()
-        val shadowBoundSide = SPAWN_BOX_SIDE * 1.1f
-        val shadowLight = DirectionalShadowLight(4096, 4096, shadowBoundSide,  shadowBoundSide, 0f, shadowBoundSide)
+        val lightDir = Vector3(0.4f, -1f, 0.8f).nor()
+        val shadowBoundSide = SPAWN_BOX_SIDE * 5f
+        val shadowLight = DirectionalShadowLight(4096, 4096, shadowBoundSide, shadowBoundSide, 0f, shadowBoundSide)
         shadowLight.set(1f, 1f, 0.9f, lightDir)
         shadowLight.intensity = 3f
 
@@ -128,9 +130,12 @@ class Test3DGame : Game() {
                 set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap))
                 set(ColorAttribute(ColorAttribute.Fog, fogColor))
                 set(FogAttribute(FogAttribute.FogEquation).set(3f, 130f, 0.8f))
+                set(PBRFloatAttribute(ShadowBias, 1 / 256f))
                 add(shadowLight)
             }
-            skyBox = SceneSkybox(envCubemap)
+            skyBox = if (Gdx.app.type == ApplicationType.Android && Gdx.app.version <= 32)
+                SceneSkybox(envCubemap, PBRShaderConfig.SRGB.NONE, null, true)
+            else SceneSkybox(envCubemap)
         }
 
         this.sceneManager = sceneManager
@@ -138,41 +143,77 @@ class Test3DGame : Game() {
     }
 
     private fun loadModels() {
-        sceneAsset = loadScene()
+        pillarSceneAsset = loadScene("models/stone_pillar.gltf")
+        cubeSceneAsset = loadScene("models/stone_cube.gltf")
     }
 
-    private fun getRandomScene(): Scene {
-        val scene = Scene(sceneAsset!!.scene)
-        setRandomTransform(scene.modelInstance.transform)
-        return scene
-    }
-
-    private fun getRandomInstancedScene(): Scene {
-        val scene = Scene(sceneAsset!!.scene)
-        enableInstancedRendering(scene.modelInstance)
-        return scene
-    }
-
-    private fun enableInstancedRendering(model: ModelInstance) {
-        val mat = Matrix4()
-        val instances =  InstanceBufferObject(true, INSTANCE_COUNT, *getMatrix4Attributes())
-        instances.numInstances = INSTANCE_COUNT
-
-        val transforms = FloatArray(INSTANCE_COUNT * MAT_FLOAT_COUNT)
-        repeat(INSTANCE_COUNT) { index ->
-            setRandomTransform(mat)
-            mat.values.copyInto(transforms, index * MAT_FLOAT_COUNT)
+    private fun addScene(
+        sceneAsset: SceneAsset,
+        count: Int,
+        transforms: Iterator<Matrix4>,
+        instanced: Boolean = USE_INSTANCED_RENDERING,
+    ) {
+        val sceneManager = this.sceneManager!!
+        if (instanced) {
+            sceneManager.addScene(createInstancedScene(sceneAsset, count, transforms))
+        } else {
+            for (mat in transforms) {
+                val scene = Scene(sceneAsset.scene)
+                scene.modelInstance.transform.set(mat)
+                sceneManager.addScene(scene)
+            }
         }
+    }
 
-        instances.setInstanceData(transforms, 0, transforms.size)
-        model.instances = instances
+    private fun createInstancedScene(sceneAsset: SceneAsset, count: Int, transforms: Iterator<Matrix4>): Scene {
+        val instances = InstanceBufferObject(true, count, *getMatrix4Attributes())
+        val floats = FloatArray(count * 16)
+
+        Gdx.app.log("TestInstance", "Start copying transforms")
+        var i = 0
+        for (mat in transforms) {
+            mat.values.copyInto(floats, i * 16, 0, 16)
+            i += 1
+        }
+        instances.setInstanceData(floats, 0, floats.size)
+        Gdx.app.log("TestInstance", "Finished copying transforms")
+
+        val scene = Scene(sceneAsset.scene)
+        scene.modelInstance.instances = instances
+        return scene
+    }
+
+    private fun getRandomTransforms(count: Int) = iterator {
+        val mat = Matrix4()
+        repeat(count) {
+            setRandomTransform(mat)
+            yield(mat)
+        }
+    }
+
+    private fun getPillarTransforms(count: Int) = iterator {
+        val mat = Matrix4()
+        repeat(count) { i ->
+            mat.idt()
+            mat.setTranslation((i / 2 - count / 2f) * 5f, -5f, 9.9f * (i % 2 - 0.5f))
+            yield(mat)
+        }
+    }
+
+    private fun getFloorTransforms(count: Int) = iterator {
+        val mat = Matrix4()
+        repeat(count) { i ->
+            mat.setToRotation(Vector3.X, 90f)
+            mat.setTranslation((i / 2 - count / 2f) * 2f, -10f, 9.9f * (i % 2 - 0.5f))
+            yield(mat)
+        }
     }
 
     private fun setRandomTransform(out: Matrix4) {
         out.setToRotation(
-            rand.nextFloat(),
-            rand.nextFloat(),
-            rand.nextFloat(),
+            rand.nextFloat() - 0.5f,
+            rand.nextFloat() - 0.5f,
+            rand.nextFloat() - 0.5f,
             rand.nextFloat() * 360f
         )
         val x = getRandomCoordinate()
@@ -201,37 +242,66 @@ class Test3DGame : Game() {
         }
     }
 
+    var timer = 1f
+
     override fun render() {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
         Gdx.gl.glEnable(GL20.GL_DEPTH_TEST)
+
         cameraController?.apply {
-            //v.set(camera.direction).nor().scl(CAMERA_SPEED * Gdx.graphics.deltaTime)
-            //target.add(v)
-            //camera.position.add(v)
+            //tmpV.set(camera.direction).nor().scl(CAMERA_SPEED * Gdx.graphics.deltaTime)
+            //target.add(tmpV)
+            //camera.position.add(tmpV)
+            target.set(camera.position)
             camera.update()
         }
+
         sceneManager?.apply {
             update(Gdx.graphics.deltaTime)
             render()
         }
-        debugLabel?.setText("$INSTANCE_COUNT instances, ${Gdx.graphics.framesPerSecond} fps, ${camera.position}")
+
+        timer -= Gdx.graphics.deltaTime
+        if (timer <= 0f) {
+            debugLabel?.setText("${sumInstances()} instances, ${Gdx.graphics.framesPerSecond} fps, ${camera.position}")
+            timer += 1f
+        }
+
         super.render()
     }
 
+    private fun sumInstances(): Int {
+        val sceneManager = sceneManager ?: return 0
+        var count = 0
+        sceneManager.renderableProviders.forEach {
+            count += if (it is Scene) it.modelInstance.instances?.numInstances ?: 1
+            else 1
+        }
+        return count
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        sceneManager?.updateViewport(
+            width.toFloat(), height.toFloat()
+        )
+    }
+
     override fun dispose() {
-        sceneAsset?.dispose()
+        pillarSceneAsset?.dispose()
     }
 
     companion object {
         const val MAT_FLOAT_COUNT = 4 * 4
-        const val CAMERA_SPEED = 16f
+        const val CAMERA_SPEED = 3f
         const val SPAWN_BOX_SIDE = 20
-        const val SPAWN_BOX_INNER_SPACE = 6
+        const val SPAWN_BOX_INNER_SPACE = 4
         const val INSTANCE_COUNT: Int = SPAWN_BOX_SIDE * SPAWN_BOX_SIDE * SPAWN_BOX_SIDE / 200
         const val DRAW_COMMAND_INT_COUNT: Int = 5
         const val DRAW_COMMAND_BYTE_COUNT: Int = DRAW_COMMAND_INT_COUNT * 4
-        const val USE_INSTANCED_RENDERING = false
+        const val USE_INSTANCED_RENDERING = true
+        const val SPAWN_FLOOR = true
 
         private fun IntBuffer.putDrawCommand(count: Int, instanceCount: Int, firstIndex: Int, baseVertex: Int) {
             // Last value must be zero (reserved)
@@ -248,8 +318,8 @@ class Test3DGame : Game() {
             }
         }
 
-        private fun loadScene(): SceneAsset {
-            return GLTFLoader().load(Gdx.files.internal("stone_pillar.gltf"))
+        private fun loadScene(gltfPath: String): SceneAsset {
+            return GLTFLoader().load(Gdx.files.internal(gltfPath))
         }
 
         private fun loadFont(): BitmapFont {
